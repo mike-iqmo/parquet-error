32c65d14587e9b617097ddccf28453a81f0e62be
mike <mike@iqmo.com>
2025-05-14 16:47:10 -0400

    Reproduce both errors

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ var db = new duckdb.Database(":memory:");
 console.log("created db");
 db.all(
   `
-    CREATE TABLE range AS FROM range(10000);
+    CREATE TABLE range AS FROM range(1250);
+    CREATE TABLE range2 as 
+    select range::VARCHAR as range_varchar from range;
 
     COPY range
-    TO 'v2.parquet'
+    TO 'duckdb_delta_binary_packed.parquet'
+    (PARQUET_VERSION v2);
+
+
+    COPY range2
+    TO 'duckdb_delta_length_byte_array.parquet'
     (PARQUET_VERSION v2);
     `,
   async function (err, res) {
@@ -17,11 +24,19 @@ db.all(
       console.warn(err);
     }
     try {
-      const asyncBuffer = await asyncBufferFromFile("v2.parquet");
+      const asyncBuffer = await asyncBufferFromFile("duckdb_delta_binary_packed.parquet");
       const objects = await parquetReadObjects({ file: asyncBuffer });
-      
+      console.log("Created",objects)
     } catch (err) {
       console.warn("Error loading parquet file", err);
     }
+
+    try {
+        const asyncBuffer = await asyncBufferFromFile("duckdb_delta_length_byte_array.parquet");
+        const objects = await parquetReadObjects({ file: asyncBuffer });
+        console.log("Created",objects)
+      } catch (err) {
+        console.warn("Error loading parquet file", err);
+      }
   }
 );
